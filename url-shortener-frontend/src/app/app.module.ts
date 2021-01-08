210b1489b851a81c582417a18b4504504b5312b1
Wendell Alves <wendellmccaulay@hotmail.com>
2021-01-07 22:59:47 -0300

    Create basic components

--- url-shortener-frontend/src/app/app.module.ts
+++ url-shortener-frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { NavComponent } from './core/nav/nav.component';
+import { ListComponent } from './core/list/list.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    ListComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
