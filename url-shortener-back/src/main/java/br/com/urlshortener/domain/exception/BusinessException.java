33a381070d37345f9223d4c661aefc92917eed1c
Wendell Alves <wendellmccaulay@hotmail.com>
2021-01-08 21:34:24 -0300

    Add domain package

---
+++ url-shortener-back/src/main/java/br/com/urlshortener/domain/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package br.com.urlshortener.domain.exception;
+
+public class BusinessException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public BusinessException(String message) {
+		super(message);
+	}
+
+}
---
+++ url-shortener-back/src/main/java/br/com/urlshortener/domain/model/User.java
@@ -0,0 +1,5 @@
+package br.com.urlshortener.domain.model;
+
+public class User {
+
+}
